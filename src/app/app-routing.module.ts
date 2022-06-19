367c7ab51b4f9b6ad10a519788a1f45308ac51a8
pola2411 <polan8103@gmail.com>
2022-06-19 09:31:41 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,38 @@
-import { NgModule } from '@angular/core';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { FaqComponent } from './faq/faq.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { TeamComponent } from './team/team.component';
+import { CtaComponent } from './cta/cta.component';
+import { ServicesComponent } from './services/services.component';
+import { SkillsComponent } from './skills/skills.component';
+import { WhyUsComponent } from './why-us/why-us.component';
+import { AboutComponent } from './about/about.component';
+import { ClientsComponent } from './clients/clients.component';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home" , component:HomeComponent},
+  {path:"" , component:HomeComponent},
+  {path:"clients" , component:ClientsComponent},
+  {path:"about" , component:AboutComponent},
+  {path:"why-us" , component:WhyUsComponent},
+  {path:"skills" , component:SkillsComponent},
+  {path:"services" , component:ServicesComponent},
+  {path:"cta" , component:CtaComponent},
+  {path:"portfolio" , component:PortfolioComponent},
+  {path:"team" , component:TeamComponent},
+  {path:"procong" , component:PricingComponent},
+  {path:"faq" , component:FaqComponent},
+  {path:"contact" , component:ContactComponent},
+  {path:"**" , component:NotfoundComponent},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,44 @@
+import { HomeComponent } from './home/home.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
+import { ClientsComponent } from './clients/clients.component';
+import { WhyUsComponent } from './why-us/why-us.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ServicesComponent } from './services/services.component';
+import { CtaComponent } from './cta/cta.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { TeamComponent } from './team/team.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { FaqComponent } from './faq/faq.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { NotfoundComponent } from './notfound/notfound.component'
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+    AboutComponent,
+    HeaderComponent,
+    ClientsComponent,
+    WhyUsComponent,
+    SkillsComponent,
+    ServicesComponent,
+    CtaComponent,
+    PortfolioComponent,
+    TeamComponent,
+    PricingComponent,
+    FaqComponent,
+    ContactComponent,
+    FooterComponent,
+    HomeComponent,
+    NotfoundComponent  
   ],
   imports: [
     BrowserModule,
